cd043d97410064e1499a452b2c0e2d469eaba2ed
zafarivaev <z.ivaev@mail.ru>
2021-07-13 22:23:07 +0500

    Initial Commit

---
+++ NetworkConnectivity/NetworkConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkConnectivityApp.swift
+//  NetworkConnectivity
+//
+//  Created by Zafar on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct NetworkConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
